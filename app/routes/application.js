793115d3cd397f8a46a5aa1340f18a4fc6420839
Bill <billvaleriote@gmail.com>
2015-02-07 13:41:35 -0800

    shuffle the deck

--- app/routes/application.js
+++ app/routes/application.js
@@ -28,9 +28,28 @@ var newDeck = function() {
 	return deck;
 };
 
+//Fisher–Yates Shuffle courtesy of bostock
+var shuffle = function (array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
 var ApplicationRoute = Ember.Route.extend({
 	model: function() {
-		return { deck: newDeck() };
+		return { deck: shuffle(newDeck()) };
 	}
 });
 
